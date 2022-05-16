defd635199614548d91ed1136efa78af8d55fd52
MINJUNG-CHO <61917990+CHO-MINJUNG@users.noreply.github.com>
2022-05-16 14:40:28 +0900

    first

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path');
+const cors = require('cors');
+
+const app = express() 
+const PORT = 8080
+
+const cafeListRouter = require('./routes/cafe_list');
+
+app.use(cors({
+  origin:true,
+  credentials:true
+}));
+app.use('/cafe/list', cafeListRouter);
+
+app.use((req, res, next) => {
+    const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+  });
+
+app.listen(PORT, () => {
+  console.log(PORT, '번 포트에서 대기 중')
+}) 
---
+++ routes/cafe_list.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
+
+const db_config = require(path.join(__dirname, '../config/database.js'));
+const connection = db_config.init();
+db_config.connect(connection);
+
+router.get('/', (req, res) => {
+  connection.query(
+    `select *
+    from cafe`,
+  (err, rows, field) => {
+    return res.send(rows);
+  })
+})
+
+
+module.exports = router;
