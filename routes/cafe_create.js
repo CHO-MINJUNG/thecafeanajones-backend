5299bcef55987eab980c1bd6fd9d8c1e8b7ce6e9
MINJUNG-CHO <61917990+CHO-MINJUNG@users.noreply.github.com>
2022-06-13 01:02:51 +0900

    create route 작성

--- routes/cafe_create.js
+++ routes/cafe_create.js
@@ -17,47 +17,51 @@ let s3 = require("./middlewares/img_s3")
 router.get('/create', isLoggedIn, (req,res) => {
 })
 
-router.post('/create', s3.upload.single('image'), (req,res) => {
-    console.log(req.file);
-//   const {title, address, filter, } = req.body;  
-//   const user_id = req.user.id;
-//   let now_office_id = null;
-//   const insert_office = {
-//     office_title: title, 
-//     thumbnail: req.files[0].location, 
-//     user_id: user_id, 
-//     user_phone: contact, 
-//     office_location: location, 
-//     address_zipcode: zipcode,
-//     address_road: road,
-//     address_detail: detail,
-//     office_deposit: deposit,
-//     office_fee: fee, 
-//     office_content: mainText
-//   }
-//   let insert_db = async () => {
-//     try{
-//       await connection.query(`INSERT INTO Office_Info SET ?`, insert_office);
+router.post('/create', isLoggedIn, s3.upload.single('image'), (req,res) => {
+  const {name, address, filter, featureSet} = req.body;  
+  const user_id = req.user.id;
 
-//       const [rows, fields] = await connection.query(`SELECT id 
-//       FROM Office_Info ORDER BY id DESC LIMIT 1;`)
+  const create_cafe = {
+    name: name,
+    thumbnail: req.file.location, 
+    address: address,
+    user_id: user_id,
+  }
+  
+  let insert_db = async () => {
+    try{
+      // cafe table에 추가
+      await connection.query(`INSERT INTO cafe SET ?`, create_cafe);
+
+      // cafe filter tabel에 추가
+      for (idx in filter){
+        console.log(filter[idx]);
+        await connection.query(`INSERT INTO cafe_filter`)
+      }
+      const [rows, fields] = await connection.query(`SELECT id 
+      FROM cafe ORDER BY id DESC LIMIT 1;`)
       
-//       now_office_id=rows[0].id;
+      now_cafe_id=rows[0].id;
 
-//       for (var img of req.files){
-//         await connection.query( 
-//           `INSERT INTO Office_Image SET ?`,
-//           {office_id: now_office_id, 
-//             file_name: img.location}
-//         )
-//       }
-//     } catch(err) {
-//       console.log(err)
-//       throw err;
-//     }
-//   }
-// insert_db()
-//   .then(res.send({createSuccess:true}))
+      // cafe feature table에 단락 추가
+      for (idx in featureSet){
+        console.log(featureSet[idx]);
+        const feature = {
+          cafe_id: now_cafe_id,
+          feature_order: idx+1,
+          user_id: user_id,
+          title: featureSet[idx].title,
+          content: featureSet[idx].content,
+        }
+        await connection.query(`INSERT INTO cafe_feature SET ?`, feature);
+      }
+    } catch(err) {
+      console.log(err)
+      throw err;
+    }
+  }
+insert_db()
+  .then(res.send({createSuccess:true}))
 
 })
 
